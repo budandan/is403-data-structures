802c2c2cf36713c0f3f3f6d37a0333c5da890f6d
Daniel Ferolino <drferolino@gmail.com>
2017-09-26 03:32:42 -0600

    Add project files.

---
+++ is403-data-structures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace is403_data_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //end program
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
